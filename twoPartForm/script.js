c3962b6502514ae4c0162b236a4c2e714ca7cfb7
Evan <truepk436@gmail.com>
2022-05-05 06:35:06 +0300

    changed files, they were wrong

--- twoPartForm/script.js
+++ twoPartForm/script.js
@@ -1,14 +1,40 @@
-function goToStepTwo(){
-    const tabs = document.getElementsByClassName('tab');
-    const steps = document.getElementsByClassName('step');
-    addRemove(tabs,'on');
-    addRemove(steps,'active');
-}
-function addRemove(arr, className){
-    arr[0].classList.remove(className);
-    arr[1].classList.add(className);
+// saving all the data into local storage to use it on the next html page
+const Allinputs = document.querySelectorAll('.multi_form .on input')
+
+function saveData(){
+    let arrKeys = [];
+    let url = 'order2.html?'
+    const selectState = document.getElementById('state');
+    //data encoded in query string and saved to be transfered to another page
+    let chosenState = encodeURIComponent(selectState.options[selectState.selectedIndex].text);
+    //saving the select data
+    url = url + selectState.name + '=' + chosenState + '&';
+    //creating key names based on input name
+    for(const name of Allinputs){
+        arrKeys.push(name.name);
+    }
+    // transfering the input data
+    for(let i = 0; i < Allinputs.length; i++){
+        url = url + encodeURIComponent(arrKeys[i]) + '=' + encodeURIComponent(Allinputs[i].value) + '&'
+    }
+    return url;
 }
 
+function simpleCheck(){
+    let valid = true;
+    for(let n = 0; n < Allinputs.length; n++){
+        if(Allinputs[n].value == ''){
+            Allinputs[n].className += ' err';
+            valid = false;
+        }
+    }
+    return valid;
+}
+localStorage.clear();
 document.getElementById('btnNext').addEventListener('click', ()=>{
-    goToStepTwo()
+    if(!simpleCheck()){
+        alert('you still have unfilled fields');
+    }else{
+    this.location.href = saveData()
+    }
 });
---
+++ twoPartForm/script2.js
@@ -0,0 +1,41 @@
+const allInputs = document.querySelectorAll('.multi_form .hidden input')
+const allergies = document.getElementById('allergies');
+console.log(allInputs);
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+function fillHiddenFields(){
+    const state = document.getElementById('state');
+    state.value = params['State'];
+    for(let i = 0; i < allInputs.length; i++){
+    //assigning the value to hidden fields by key in the params object 
+    allInputs[i].value = params[allInputs[i].name];
+     console.log(allInputs[i].name);   
+    }
+}
+fillHiddenFields();
+// function getCurrentFormData()
+// {
+//     const frequencyVal = document.querySelector('input[name="Frequency"]:checked').value
+//     const package_sizeVal = document.querySelector('input[name="size"]:checked').value
+//     const allergies = document.getElementById('allergies').value;
+//     return frequencyVal, package_sizeVal, allergies
+// }
+
+for(const n of allInputs)
+{
+    console.log(n.value);
+}
+document.querySelector('.multi_form').addEventListener('submit', (e) => {
+    // e.preventDefault();
+    fillHiddenFields();
+    logToConsole();
+    console.log();
+});     
+console.log(params);
+//loging out the entered data
+(function(){
+    let queryString = decodeURIComponent(window.location.search);
+    console.log("You've entered following data:");
+    console.log(queryString.replaceAll('&', '\n').slice(1));
+})()
